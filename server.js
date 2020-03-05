93c7846a5e719cc8dfe51ff344c09602984225e3
Briana Bricher <bbricher@udallas.edu>
2020-03-05 16:09:32 -0600

    attempt number 2

--- server.js
+++ server.js
@@ -2,15 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const path = require('path');
+var MongoClient = require('mongodb').MongoClient;
 
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://briana_bricher:noPAIN123@cluster0-mzd5a.gcp.mongodb.net/test?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+var uri = "mongodb://briana_bricher:noPAIN123@cluster0-shard-00-00-mzd5a.gcp.mongodb.net:27017,cluster0-shard-00-01-mzd5a.gcp.mongodb.net:27017,cluster0-shard-00-02-mzd5a.gcp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority";
 
 const items = require('./routes/api/items');
 
@@ -21,9 +15,15 @@ app.use(bodyParser.json());
 
 // Connect to Mongo
 mongoose
-    .connect(process.env.MONGODB_URI || "mongodb+srv://briana_bricher:noPAIN123@cluster0-mzd5a.gcp.mongodb.net/test?retryWrites=true&w=majority")
-    .then(() => console.log('MongoDB Connected...'))
-    .catch(err => console.log(err));
+.connect(process.env.MONGODB_URI || "mongodb+srv://briana_bricher:noPAIN123@cluster0-mzd5a.gcp.mongodb.net/test?retryWrites=true&w=majority")
+.then(() => console.log('MongoDB Connected...'))
+.catch(err => console.log(err));
+
+MongoClient.connect(process.env.MONGODB_URI || uri, function(err, client) {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
 // Use Routes
 app.use('/api/items', items);
