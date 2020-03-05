eb2e7982b2fc267f63bacc2068602c1ce023cffe
Briana Bricher <bbricher@udallas.edu>
2020-03-05 15:18:43 -0600

    Full working shopping list app, next to deploy to heroku

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
 import AppNavbar from './Components/AppNavbar';
 import ShoppingList from './Components/ShoppingList';
+import ItemModal from './Components/ItemModal';
+import { Container } from 'reactstrap';
 
+import { Provider } from 'react-redux';
+import store from './store';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <AppNavbar/>
-      <ShoppingList/>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <AppNavbar/>
+        <Container>
+          <ItemModal/>
+          <ShoppingList/>
+        </Container>
+      </div>
+    </Provider>
   );
 }
 
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const items = require('./routes/api/items');
 
@@ -11,12 +12,22 @@ app.use(bodyParser.json());
 
 // Connefct ot Mongo
 mongoose
-    .connect(process.env.MONGODB_URI || "mongodb://localhost/shoppingList")
+    .connect(process.env.MONGODB_URI || "mongodb+srv://briana_bricher:noPAIN123@cluster0-mzd5a.gcp.mongodb.net/test?retryWrites=true&w=majority")
     .then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err));
 
 // Use Routes
 app.use('/api/items', items);
+
+// Serve static assest if we're in production
+if(process.env.NODE_ENV === 'production') {
+    //Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const port = process.env.PORT || 5000;
 
