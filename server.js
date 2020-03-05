1d3a2f1337ef8c7061c86a3fdd259055fc58aaa1
Briana Bricher <bbricher@udallas.edu>
2020-03-05 17:02:51 -0600

    attemp number 4

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const path = require('path');
-var MongoClient = require('mongodb').MongoClient;
+// var MongoClient = require('mongodb').MongoClient;
 
-var uri = process.env.MONGODB_URI || "mongodb://briana_bricher:noPAIN123@cluster0-shard-00-00-mzd5a.gcp.mongodb.net:27017,cluster0-shard-00-01-mzd5a.gcp.mongodb.net:27017,cluster0-shard-00-02-mzd5a.gcp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority";
+var MONGODB_URI = "mongodb://briana_bricher:noPAIN123@cluster0-shard-00-00-mzd5a.gcp.mongodb.net:27017,cluster0-shard-00-01-mzd5a.gcp.mongodb.net:27017,cluster0-shard-00-02-mzd5a.gcp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority";
 
 const items = require('./routes/api/items');
 
@@ -15,15 +15,18 @@ app.use(bodyParser.json());
 
 // Connect to Mongo
 mongoose
-.connect(process.env.MONGODB_URI || "mongodb+srv://briana_bricher:noPAIN123@cluster0-mzd5a.gcp.mongodb.net/test?retryWrites=true&w=majority")
+.connect(process.env.MONGODB_URI || "mongodb+srv://briana_bricher:noPAIN123@cluster0-mzd5a.gcp.mongodb.net/test?retryWrites=true&w=majority",
+    {useNewUrlParser: true,
+    useUnifiedTopology: true}
+)
 .then(() => console.log('MongoDB Connected...'))
 .catch(err => console.log(err));
 
-MongoClient.connect(uri, function(err, client) {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+// MongoClient.connect(MONGODB_URI, function(err, client) {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
 
 // Use Routes
 app.use('/api/items', items);
